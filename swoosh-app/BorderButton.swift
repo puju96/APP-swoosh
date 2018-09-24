c33b2437ded2f4a343224b0f81355a732d511c88
Admin <admin@Admins-Mac-2.local>
2018-09-24 14:59:16 +0530

    all screens are added

---
+++ swoosh-app/BorderButton.swift
@@ -0,0 +1,28 @@
+//
+//  BorderButton.swift
+//  swoosh-app
+//
+//  Created by Admin on 02/07/1940 Saka.
+//  Copyright © 1940 Admin. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
